87b0e0be3405be16548f7860128d7472ae92e168
NiPeGun <nipegun@gmail.com>
2021-06-01 10:29:35 +0200

    Create OpenWrtX86-Instalar.sh

---
+++ OpenWrtX86-Instalar.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#--------------------------------------------------------------------------------------------
+#  Script de NiPeGun para instalar OpenWrt en un ordenador UEFI iniciando desde Ubuntu Live 
+#--------------------------------------------------------------------------------------------
+
+ColorVerde="\033[1;32m"
+FinColor="\033[0m"
+
+echo ""
+echo -e "${ColorVerde}Iniciando el script de instalación de OpenWrt en ordenador UEFI...${FinColor}"
+echo ""
+
+
+
+echo ""
+echo -e "${ColorVerde}Script de instalación de OpenWrt en ordenador UEFI, finalizado${FinColor}"
+echo ""
