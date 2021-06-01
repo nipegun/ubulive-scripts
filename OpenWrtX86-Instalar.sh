b82851cd1fa7634d64626c798801f790dd430e3a
NiPeGun <nipegun@gmail.com>
2021-06-01 10:42:04 +0200

    Update OpenWrtX86-Instalar.sh

--- OpenWrtX86-Instalar.sh
+++ OpenWrtX86-Instalar.sh
@@ -34,13 +34,13 @@ echo ""
 echo ""
 echo "Creando carpetas para montar las particiones..."
 echo ""
-sudo mkdir -p /OpenWrt/PartEFI/
+sudo mkdir -p /OpenWrt/PartOVMF/
 sudo mkdir -p /OpenWrt/PartExt4/
 
 echo ""
 echo "Montando las particiones..."
 echo ""
-sudo mount -t auto /dev/sda1 /OpenWrt/PartEFI/
+sudo mount -t auto /dev/sda1 /OpenWrt/PartOVMF/
 sudo mount -t auto /dev/sda2 /OpenWrt/PartExt4/
 
 echo ""
@@ -59,18 +59,19 @@ sudo mkdir -p /OpenWrt/PartExt4/boot
 ## Comprobar si el paquete wget está instalado. Si no lo está, instalarlo.
    if [[ $(dpkg-query -s wget 2>/dev/null | grep installed) == "" ]]; then
      echo ""
-     echo "wget no está instalado. Iniciando su instalación..."
+     echo "  wget no está instalado. Iniciando su instalación..."
      echo ""
      sudo apt-get -y update
      sudo apt-get -y install wget
+     echo ""
    fi
 sudo wget --no-check-certificate https://downloads.openwrt.org/releases/$VersOpenWrt/targets/x86/64/openwrt-$VersOpenWrt-x86-64-vmlinuz -O /OpenWrt/PartExt4/boot/vmlinuz
 
 echo ""
 echo "Bajando el archivo con el sistema root..."
 echo ""
-rm -rf /OpenWrt/PartEFI/rootfs.tar.gz
-sudo wget --no-check-certificate https://downloads.openwrt.org/releases/$VersOpenWrt/targets/x86/64/openwrt-$VersOpenWrt-x86-64-generic-rootfs.tar.gz -O /OpenWrt/PartEFI/rootfs.tar.gz
+rm -rf /OpenWrt/PartOVMF/rootfs.tar.gz
+sudo wget --no-check-certificate https://downloads.openwrt.org/releases/$VersOpenWrt/targets/x86/64/openwrt-$VersOpenWrt-x86-64-generic-rootfs.tar.gz -O /OpenWrt/PartOVMF/rootfs.tar.gz
 
 echo ""
 echo "Descomprimiendo el sistema de archivos root en la partición ext4..."
@@ -78,10 +79,11 @@ echo ""
 ## Comprobar si el paquete tar está instalado. Si no lo está, instalarlo.
    if [[ $(dpkg-query -s tar 2>/dev/null | grep installed) == "" ]]; then
      echo ""
-     echo "tar no está instalado. Iniciando su instalación..."
+     echo "  tar no está instalado. Iniciando su instalación..."
      echo ""
      sudo apt-get -y update
      sudo apt-get -y install tar
+     echo ""
    fi
 sudo tar -xf /OpenWrt/PartEFI/rootfs.tar.gz -C /OpenWrt/PartExt4/
 
@@ -89,20 +91,20 @@ echo ""
 echo "Configurando la MV de OpenWrt para que pille IP por DHCP"
 echo ""
 sudo rm -rf /OpenWrt/PartExt4/etc/config/network
-sudo cp /OpenWrt/PartEFI/scripts/network /OpenWrt/PartExt4/etc/config/
+sudo cp /OpenWrt/PartOVMF/scripts/network /OpenWrt/PartExt4/etc/config/
 
 echo ""
 echo "Copiando el script de instalación de paquetes..."
 echo ""
 sudo mkdir -p /OpenWrt/PartExt4/root/scripts/
-sudo cp /OpenWrt/PartEFI/scripts/1-InstalarPaquetes.sh /OpenWrt/PartExt4/root/scripts/1-InstalarPaquetes.sh
-sudo chmod +x                                        /OpenWrt/PartExt4/root/scripts/1-InstalarPaquetes.sh
+sudo cp /OpenWrt/PartOVMF/scripts/1-InstalarPaquetes.sh /OpenWrt/PartExt4/root/scripts/1-InstalarPaquetes.sh
+sudo chmod +x                                           /OpenWrt/PartExt4/root/scripts/1-InstalarPaquetes.sh
 
 echo ""
 echo "Copiando el script de instalación de los o-scripts..."
 echo ""
-sudo cp /OpenWrt/PartEFI/scripts/2-InstalarOScripts.sh /OpenWrt/PartExt4/root/scripts/2-InstalarOScripts.sh
-sudo chmod +x /OpenWrt/PartExt4/root/scripts/2-InstalarOScripts.sh
+sudo cp /OpenWrt/PartOVMF/scripts/2-InstalarOScripts.sh /OpenWrt/PartExt4/root/scripts/2-InstalarOScripts.sh
+sudo chmod +x                                           /OpenWrt/PartExt4/root/scripts/2-InstalarOScripts.sh
 
 
 echo ""
