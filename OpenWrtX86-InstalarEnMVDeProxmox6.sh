da6e6a67492cf8995f2748f67d93ccf4d71cc042
NiPeGun <nipegun@gmail.com>
2021-06-01 15:05:28 +0200

    Update and rename OpenWrtX86-InstalarEnMVDeProxmox5.sh to OpenWrtX86-InstalarEnMVDeProxmox6.sh

--- OpenWrtX86-InstalarEnMVDeProxmox6.sh
+++ OpenWrtX86-InstalarEnMVDeProxmox6.sh
@@ -161,9 +161,9 @@ menu=(dialog --timeout 5 --checklist "Instalación de OpenWrt X86:" 22 90 16)
           sudo su -c "echo ''                                                                                                                         >> /OpenWrt/PartOVMF/EFI/OpenWrt/grub.cfg"
           sudo su -c "echo 'set default="'"0"'"'                                                                                                      >> /OpenWrt/PartOVMF/EFI/OpenWrt/grub.cfg"
           sudo su -c "echo 'set timeout="'"1"'"'                                                                                                      >> /OpenWrt/PartOVMF/EFI/OpenWrt/grub.cfg"
-          sudo su -c "echo ""set root=''(hd0,2)''""                                                                                                   >> /OpenWrt/PartOVMF/EFI/OpenWrt/grub.cfg"
+          sudo su -c 'echo "set root=''(hd0,2)''"                                                                                                     >> /OpenWrt/PartOVMF/EFI/OpenWrt/grub.cfg'
           sudo su -c "echo ''                                                                                                                         >> /OpenWrt/PartOVMF/EFI/OpenWrt/grub.cfg"
-          sudo su -c "echo 'menuentry ""OpenWrt"" {'                                                                                                  >> /OpenWrt/PartOVMF/EFI/OpenWrt/grub.cfg"
+          sudo su -c "echo 'menuentry "'"OpenWrt"'" {'                                                                                                >> /OpenWrt/PartOVMF/EFI/OpenWrt/grub.cfg"
           sudo su -c "echo '  linux /boot/vmlinuz root=/dev/sda2 rootfstype=ext4 rootwait console=tty0 console=ttyS0,115200n8 noinitrd'               >> /OpenWrt/PartOVMF/EFI/OpenWrt/grub.cfg"
           sudo su -c "echo '}'                                                                                                                        >> /OpenWrt/PartOVMF/EFI/OpenWrt/grub.cfg"
           sudo su -c "echo 'menuentry "'"OpenWrt (failsafe)"'" {'                                                                                     >> /OpenWrt/PartOVMF/EFI/OpenWrt/grub.cfg"
