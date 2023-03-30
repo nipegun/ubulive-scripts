e90ff13b0462cf5361ca4e227ce34d22db2e1baa
NiPeGun <nipegun@gmail.com>
2023-03-30 03:25:09 +0200

    Update OpenWrtX86-Particiones-Montar.sh

--- OpenWrtX86-Particiones-Montar.sh
+++ OpenWrtX86-Particiones-Montar.sh
@@ -5,12 +5,12 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
-#-----------------------------------------------------------------------------------------------------------------
+# ----------
 #  Script de NiPeGun para montar las particiones de OpenWrt en Ubuntu Live 
 #
 # Ejecución remota:
 # curl -s https://raw.githubusercontent.com/nipegun/ubulive-scripts/main/OpenWrtX86-Particiones-Montar.sh | bash
-#-----------------------------------------------------------------------------------------------------------------
+# ----------
 
 # Crear las carpetas para montar las particiones
   echo ""
